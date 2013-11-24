dc657a8df4dcdfc12df312b10decd3905f15ff5e
heruku <ukutaht@Ukus-MacBook-Pro.local>
2013-11-24 02:18:54 -0600

    extracted a helper function to get query string params

--- js/main.js
+++ js/main.js
@@ -16,22 +16,19 @@ SoundStripeView.prototype.render = function() {
 }
 
 function App() {
-  var photoUrlResults = /photoUrl=(.*)&/.exec(window.location.href);
-  var photoUrl;
-  if(photoUrlResults && photoUrlResults.length > 1) {
-    photoUrl = photoUrlResults[1];
-  }
-
-  var songUrl;
-  var songUrlResults = /songUrl=(.*)(&|$)/.exec(window.location.href);
-  if(songUrlResults && songUrlResults.length > 1 ) {
-    songUrl = songUrlResults[1];
-  }
+  photoUrl = getQueryParam("photoUrl");
+  songUrl = getQueryParam("songUrl");
 
   this.model = new SoundStripe(photoUrl, songUrl);
 
   this.view = new SoundStripeView("#sound-stripe", this.model);
   this.view.render();
+}
+
+function getQueryParam(paramName) {
+  var regex = new RegExp(paramName + "=([^&]*)")
+  var results = regex.exec(window.location.href);
+  return results[1]
 }
 
 
