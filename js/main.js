cc1e57c02dd391ba4576fb85629d4226fc647253
Nate Delage <nate@natedelage.com>
2013-11-25 09:44:54 -0600

    Add SoundStripeFormView

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ function SoundStripe(photoUrl, songUrl) {
   this.songUrl = songUrl;
 }
 
+SoundStripe.prototype.valid = function() {
+  return this.photoUrl && this.songUrl;
+}
+
 function SoundStripeView(el, model) {
   this.$el = el;
   this.model = model;
@@ -12,6 +16,25 @@ function SoundStripeView(el, model) {
 
 SoundStripeView.prototype.render = function() {
   var html = this.soundStripeTemplate(this.model);
+  this.$el.html(html);
+}
+
+function SoundStripeFormView(el, model) {
+  this.$el = el;
+  this.model = model;
+  this.template = _.template(Templates.soundStripeForm);
+
+  this.$el.on('submit', 'form', this.updateModel.bind(this));
+}
+
+SoundStripeFormView.prototype.updateModel = function(e) {
+  e.preventDefault();
+  this.model.photoUrl = this.$el.find("#photoUrl").val();
+  this.model.songUrl = this.$el.find("#songUrl").val();
+}
+
+SoundStripeFormView.prototype.render = function() {
+  var html = this.template(this.model);
   this.$el.html(html);
 }
 
@@ -46,8 +69,14 @@ function App() {
 
   this.model = new SoundStripe(photoUrl, songUrl);
 
-  this.view = new SoundStripeView($("#sound-stripe"), this.model);
-  this.view.render();
+  if(this.model.valid()) {
+    this.view = new SoundStripeView($("#sound-stripe"), this.model);
+    this.view.render();
+  } else {
+    this.view = new SoundStripeFormView($("#new-sound-stripe"), this.model);
+    this.view.render();
+  }
+
 }
 
 
--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,9 @@
 var Templates = {
   soundStripe: '<img id="photo" src="<%=photoUrl%>">\
-                <audio id="song" autoplay src="<%=songUrl%>"></audio>'
+                <audio id="song" autoplay src="<%=songUrl%>"></audio>',
+  soundStripeForm: '<form action="#">\
+                      <input type="text" id="photoUrl" /><br /> \
+                      <input type="text" id="songUrl" /><br /> \
+                      <input type="submit" val="Generate URL" /> \
+                    </form>'
 }
